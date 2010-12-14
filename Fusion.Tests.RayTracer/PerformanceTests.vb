d8e4658669f3676c6a319170f80f4168ea85720b
Julius Kunze <juliuskunze@gmail.com>
2010-12-14 19:35:44 +0100

    Unoptimized

---
+++ Fusion.Tests.RayTracer/PerformanceTests.vb
@@ -0,0 +1,14 @@
+﻿Public Class PerformanceTests
+
+    <Test()>
+    Public Sub ExamplePicture()
+        Dim startTime = Now
+
+        Dim picture = New Ry.RayTracingExamples(pictureSize:=New Size(100, 100)).IluminationRoom.Picture
+
+        Dim neededTime = Now - startTime
+
+        Assert.Less(neededTime, TimeSpan.FromSeconds(0.8D), message:=neededTime.ToString)
+    End Sub
+
+End Class
